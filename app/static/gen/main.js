93beef92a1951e30ad2c02af03ea478e6afea0b1
rmallach <mallachr19@gmail.com>
2020-07-24 15:18:09 -0400

    HTML edit

--- app/static/gen/main.js
+++ app/static/gen/main.js
@@ -131,17 +131,3 @@ function makeSimLine(parentName, SVG_name, dataType=null, simA=null, simB=null, 
        .attr("text-anchor", "middle")
        .text(title);
 }
-
-
-
-function format_datetime(date){
-  month = '' + (date.getMonth() + 1),
-  day = '' + date.getDate(),
-  year = date.getFullYear();
-  if (month.length < 2)
-    month = '0' + month;
-  if (day.length < 2)
-    day = '0' + day;
-  date = [year, month, day].join('-');
-  return date
-}
