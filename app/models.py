a50c79193384af09acd34ff16d7340e578b3cae2
rmallach <mallachr19@gmail.com>
2020-07-24 14:06:07 -0400

    HTML edit

--- app/models.py
+++
@@ -1,9 +0,0 @@
-# from app import db
-
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.Text(), nullable=False)
-
-#     def __repr__(self):
-#         return '<User: {}>'.format(self.name)
