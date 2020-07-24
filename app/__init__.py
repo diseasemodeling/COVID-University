3c9650a7413dd347a9c0e687f96c4e7e2a858c8a
rmallach <mallachr19@gmail.com>
2020-07-24 14:36:23 -0400

    remove unused

--- app/__init__.py
+++ app/__init__.py
@@ -2,23 +2,10 @@ import os
 from flask import Flask
 from flask_assets import Bundle, Environment
 
-
-
-bundles = {
-        'd3_viz':Bundle('map.js', 'foresight.js', 'covid-line.js',
-                        'userInput-line.js', 'inputlineABC.js', 'policy_builder.js',
-                        'simResult.js',
-                        output='gen/main.js'),
-
-        'home_css':Bundle('home.css',
-                          output='gen/main.css'),
-}
-
-ALLOWED_EXTENSIONS = {'csv', 'xls', 'xlsx'}
+bundles = {'javascript':Bundle('simResult.js', output='gen/main.js'),}
 
 def create_app():
     app = Flask(__name__)
-
 
     assets = Environment(app)
     assets.register(bundles)
